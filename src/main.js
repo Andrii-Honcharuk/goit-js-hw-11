3faa65ff600e88e375f897ea78ac613fe0a22903
mag-17 <132659969+mag-17@users.noreply.github.com>
2024-01-05 22:01:02 +0200

    hw-11 v2
    change js - change sent-url, renderHtmlFunction, remove last find

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-// Описаний в документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 import iziToast from 'izitoast';
@@ -14,17 +12,32 @@ const galleryContainer = document.querySelector(".gallery");
 
 const loader = document.querySelector(".loader");
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  close: true,
+  enableKeyboard: true,
+  docClose: true,
+});
+
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const query = event.currentTarget.elements.query.value
+  const query = event.currentTarget.elements.query.value.trim()
   
-  url.searchParams.append("key", "41612762-752dc9341b43071862b7b3b8c")
-  url.searchParams.append("q", query)
-  url.searchParams.append("image_type", "photo")
-  url.searchParams.append("orientation", "horizontal");
-  url.searchParams.append("safesearch", true);
+  const params = {
+    key: '41612762-752dc9341b43071862b7b3b8c',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+};
+
+url.search = new URLSearchParams(params);
   
+  galleryContainer.innerHTML = ''; // очищення при кожному натисканні
+
   loader.style.display = "block";
 
     fetch(url)
@@ -36,19 +49,13 @@ searchForm.addEventListener("submit", (event) => {
     })
     .then(imgs => {
       if (imgs.hits.length === 0) {
-
         iziToast.error({
           title: 'Error',
           position: 'center',
           message: 'Sorry, there are no images matching your search query. Please try again!',
         });
       } else {
-      galleryContainer.innerHTML = imgs
-        .hits
-        .map((img) => {
-          return `<li class="img-gallery-item"><a class="gallery-link" href="${img.largeImageURL}"><img class="gallery-image" src="${img.webformatURL}" alt="${img.tags}" width="${img.webformatWidth}"></a></li>`;
-        })
-        .join(""); 
+        galleryContainer.innerHTML = renderImagesHtmlReduce(imgs.hits)
   
     lightbox.refresh(); 
   }
@@ -67,12 +74,18 @@ searchForm.addEventListener("submit", (event) => {
 
 });
 
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  nav: true,
-  captionDelay: 250,
-  captionsData: 'alt',
-  close: true,
-  enableKeyboard: true,
-  docClose: true,
-});
+function renderImagesHtmlReduce(galleryContainer) {
+  return galleryContainer.reduce(
+    (acc, img) =>
+      acc +
+      `<li class="img-gallery-item"><a class="gallery-link" href="${img.largeImageURL}"><img class="gallery-image" src="${img.webformatURL}" alt="${img.tags}" width="${img.webformatWidth}"></a>
+        <div class="info-img">
+          <p>Likes<span>${img.likes}</span></p>
+          <p>Views<span>${img.views}</span></p>
+          <p>Comments<span>${img.comments}</span></p>
+          <p>Downloads<span>${img.downloads}</span></p>
+        </div>
+      </li>`,
+    ''
+  );
+}
